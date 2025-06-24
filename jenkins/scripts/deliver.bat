ff69a29a1a7822394a916bafe160f6d4f78eda80
raulmegi <mat.raulmegias@gmail.com>
2025-06-24 18:05:11 +0200

    Ajustado Jenkinsfile y deliver.bat con ruta absoluta de Maven v3

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -1,22 +1,32 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM Ruta completa a Maven
+REM Ruta absoluta a Maven
 set MAVEN=C:\apache-maven-3.9.10\bin\mvn
 
 echo Instalando artefacto en el repositorio local de Maven...
 call "%MAVEN%" install
 
-REM Extraer name y version desde pom.xml
+REM Inicializar variables
 set NAME=
 set VERSION=
 
+REM Extraer <name> desde pom.xml
 for /f "tokens=1,* delims=>" %%A in ('findstr "<name>" pom.xml') do (
-    for /f "tokens=1 delims=<" %%C in ("%%B") do set NAME=%%C
+    if not defined NAME (
+        for /f "tokens=1 delims=<" %%C in ("%%B") do set NAME=%%C
+    )
 )
 
+REM Extraer la PRIMERA <version> (la del proyecto, no la del plugin)
+set FOUND_VERSION_LINE=0
 for /f "tokens=1,* delims=>" %%A in ('findstr "<version>" pom.xml') do (
-    for /f "tokens=1 delims=<" %%C in ("%%B") do set VERSION=%%C
+    if !FOUND_VERSION_LINE! == 0 (
+        for /f "tokens=1 delims=<" %%C in ("%%B") do (
+            set VERSION=%%C
+            set FOUND_VERSION_LINE=1
+        )
+    )
 )
 
 echo Nombre del proyecto: !NAME!
